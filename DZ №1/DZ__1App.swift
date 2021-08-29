f09357458a84a6aaf993d98a0012d55b95d48bf2
Egor Malyshev <egor.malyshev@umbrellait.com>
2021-08-29 18:10:08 +0300

    Initial Commit

---
+++ DZ №1/DZ__1App.swift
@@ -0,0 +1,17 @@
+//
+//  DZ__1App.swift
+//  DZ №1
+//
+//  Created by Egor Malyshev on 29.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct DZ__1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
